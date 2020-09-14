781036551d1661fa3b65f2e03898fbdb87ccca55
Yago Lopez <yago.lopez@gmail.com>
2020-09-15 01:18:16 +0200

    Get data from database using prisma client

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
 
-export default (req, res) => {
+export default async (req, res) => {
+  const prisma = new PrismaClient()
+  const users = await prisma.user.findMany()
+
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json(users)
 }

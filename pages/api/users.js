e6a3cf2777287ccc719b07df226c7d369a244f8d
Yago Lopez <yago.lopez@gmail.com>
2020-09-17 20:13:24 +0200

    Tests

--- pages/api/users.js
+++ pages/api/users.js
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 export default async (req, res) => {
   try {
     const prisma = new PrismaClient()
-    // const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany()
     // res.status(200).json(users)
     res.status(200).json({"test": "hola"})
 

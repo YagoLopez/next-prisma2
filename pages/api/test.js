99f7140f911ebbcf8af8a1e49351cb0a31eb7e71
Yago Lopez <yago.lopez@gmail.com>
2020-09-17 19:59:11 +0200

    Refactor /api/users endpoint

---
+++ pages/api/test.js
@@ -0,0 +1,23 @@
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, () => {})
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
+
+export default handler

cf7c29f0640bcffc85b04cbf432f480bafaab839
Yago Lopez <yago.lopez@gmail.com>
2020-09-17 19:39:44 +0200

    Refactor /api/users endpoint

--- pages/api/users.js
+++ pages/api/users.js
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 export default async (req, res) => {
   try {
-    // const prisma = new PrismaClient()
+    const prisma = new PrismaClient()
     // const users = await prisma.user.findMany()
     // res.status(200).json(users)
     res.status(200).json({"test": "hola"})
